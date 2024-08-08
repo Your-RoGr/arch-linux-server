5c98586f9ed2d1bf1258574004e1c42a667d0172
RoGr <rogryour.teamlead@gmail.com>
2024-08-09 03:30:49 +0500

    v0.0.2c

--- docker/compose.sh
+++ docker/compose.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sudo mkdir -p /mnt/raid0/etc/letsencryp
+sudo mkdir -p /mnt/raid0/var/lib/letsencrypt
+sudo mkdir -p /mnt/raid0/extrolabs-db-data
+sudo mkdir -p /mnt/raid0/var/gogs
+
+sudo chown -R $USER /mnt/raid0/etc/letsencryp
+sudo chown -R $USER /mnt/raid0/var/lib/letsencrypt
+sudo chown -R $USER /mnt/raid0/extrolabs-db-data
+sudo chown -R $USER /mnt/raid0/var/gogs
+
 cd "$(dirname $0)"
 sudo mkdir -p ./nginx
 sudo touch ./nginx/error.log
