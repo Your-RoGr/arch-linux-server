c15de66d21c4be49823bdada6e46c28bf206005b
RoGr <rogryour.teamlead@gmail.com>
2024-08-09 03:19:33 +0500

    v0.0.2a

--- docker/compose.sh
+++ docker/compose.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Enter your raid or send nothing for use root (example: /mnt/raid0):"
-read path
-path=$(echo $path | sed 's/[^[:alnum:]\/_:]//g')
-echo "You entered: $path"
-
 cd "$(dirname $0)"
 sudo mkdir -p ./nginx
 sudo touch ./nginx/error.log
