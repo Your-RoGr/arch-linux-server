4babf20c9c8ab5acaddab453d423f71a1bfa7f1d
RoGr <rogryour.teamlead@gmail.com>
2024-08-09 03:34:49 +0500

    v0.0.2d

--- docker/compose.sh
+++ docker/compose.sh
@@ -2,12 +2,12 @@
 
 sudo mkdir -p /mnt/raid0/etc/letsencryp
 sudo mkdir -p /mnt/raid0/var/lib/letsencrypt
-sudo mkdir -p /mnt/raid0/extrolabs-db-data
+sudo mkdir -p /mnt/raid0/var/lib/postgresql/data
 sudo mkdir -p /mnt/raid0/var/gogs
 
 sudo chown -R $USER /mnt/raid0/etc/letsencryp
 sudo chown -R $USER /mnt/raid0/var/lib/letsencrypt
-sudo chown -R $USER /mnt/raid0/extrolabs-db-data
+sudo chown -R $USER /mnt/raid0/var/lib/postgresql/data
 sudo chown -R $USER /mnt/raid0/var/gogs
 
 cd "$(dirname $0)"
