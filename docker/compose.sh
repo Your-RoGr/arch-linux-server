7c7e8fe6a78b5b89c9dc0cdbe8994c02ca8480ca
RoGr <rogryour.teamlead@gmail.com>
2024-08-09 19:46:39 +0500

    v0.0.2g

--- docker/compose.sh
+++ docker/compose.sh
@@ -4,11 +4,13 @@ sudo mkdir -p /mnt/raid0/etc/letsencryp
 sudo mkdir -p /mnt/raid0/var/lib/letsencrypt
 sudo mkdir -p /mnt/raid0/var/lib/postgresql/data
 sudo mkdir -p /mnt/raid0/var/gogs
+sudo mkdir -p /mnt/raid0/var/www/certbot
 
 sudo chown -R $USER /mnt/raid0/etc/letsencryp
 sudo chown -R $USER /mnt/raid0/var/lib/letsencrypt
 sudo chown -R $USER /mnt/raid0/var/lib/postgresql/data
 sudo chown -R $USER /mnt/raid0/var/gogs
+sudo chown -R $USER /mnt/raid0/var/www/certbot
 
 cd "$(dirname $0)"
 sudo mkdir -p ./nginx
