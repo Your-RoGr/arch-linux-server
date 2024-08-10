d997e01a5639e64c958bbb310261656f61784296
RoGr <rogryour.teamlead@gmail.com>
2024-08-10 19:19:52 +0500

    v0.0.5

--- docker/compose.sh
+++ docker/compose.sh
@@ -8,7 +8,7 @@ cd "$(dirname $0)"
 # domains=("example.com www.example.com" "another-example.com www.another-example.com")
 domains=("gogs.extrolabs.ru")
 email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 data_path="./data/certbot"
 rsa_key_size=4096
